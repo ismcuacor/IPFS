06b901debc042cffd7f6e8f656b00561e3b0b6e0
Ismael Cuadrado-Cordero <32838715+ismcuacor@users.noreply.github.com>
2020-05-15 13:28:05 +0200

    Update HTTP.go

--- docs/examples/ipfs-crawler/HTTP.go
+++ docs/examples/ipfs-crawler/HTTP.go
@@ -107,7 +107,7 @@ func findClosestPeersHTTP(peerID string) {
 		}
 		if _,hit := peersMap[nextPeer.ID]; !hit {
 			// Add the peer to the list of nodes to visit in the next Iteration
-			peersList.PushBack(nextPeer.ID.Pretty())
+			peersList.PushBack(nextPeer)
 		}
 		
 		peersMap[nextPeer.ID] = 1
