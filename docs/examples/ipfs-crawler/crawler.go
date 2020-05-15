9ca8b47d6d8e1397fbe0d50be89a9426db777e63
Ismael Cuadrado-Cordero <32838715+ismcuacor@users.noreply.github.com>
2020-05-15 13:15:19 +0200

    Update crawler.go

--- docs/examples/ipfs-crawler/crawler.go
+++ docs/examples/ipfs-crawler/crawler.go
@@ -159,7 +159,7 @@ func main() {
 		for peer := peersList.Front(); peer != nil; peer = peer.Next() {
 			fmt.Println("checking peer: " + peer.Value.(string))
 			if _,hit := peersMap[peer.Value.(string)]; !hit {
-				peersMap[peer.Pretty()] = 1
+				peersMap[peer.Value.Pretty()] = 1
 				findClosestPeersAPI(peer.Value.(string))
 				//findClosestPeersHTTP(peer.Value.(string))
 			}
